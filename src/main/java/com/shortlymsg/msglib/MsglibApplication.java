ca2e3208d0e2160c55aef89484570b6348d30697
ShortlyMsg <42msg42@gmail.com>
2023-01-20 18:19:31 +0300

    Initial commit, kotlin support added

---
+++ src/main/java/com/shortlymsg/msglib/MsglibApplication.java
@@ -0,0 +1,13 @@
+package com.shortlymsg.msglib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsglibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsglibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shortlymsg/msglib/MsglibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortlymsg.msglib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsglibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
